434c275afa0c3397cadbeee8639479fefa268740
Bryan Barnes <bryan.barnes12@gmail.com>
2021-06-17 21:15:17 -0500

    Adding the models

---
+++ models/Role.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Role extends Model {}
+
+Role.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'role',
+  }
+);
+
+module.exports = Role;

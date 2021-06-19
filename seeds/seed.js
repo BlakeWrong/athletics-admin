3d8dd1525fd805c0d74bbea678164986837e3c46
gannacon <cwgannaway@gmail.com>
2021-06-19 12:50:25 -0500

    mess with seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,25 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Team, Role } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const roleData = require('./roleData.json');
+const teamData = require('./teamData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  Team.bulkCreate(teamData, {
+    individualHooks: true,
+    returning: true,
+  });
+  Role.bulkCreate(roleData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };

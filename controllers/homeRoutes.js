8088c5f06631c4c6818570bdc25815b0a5dfbb60
Blake <btdubbzzz@gmail.com>
2021-06-19 11:13:14 -0500

    setting up dummy routes for page rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,70 +3,35 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    //route to render home page
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/admin', async (req, res) => {
+    //route to render admin console
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
+router.get('/users', async (req, res) => {
+    //route to render all users
+});
 
-    const user = userData.get({ plain: true });
+router.get('/teams', async (req, res) => {
+    //route to render all teams
+});
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/teams/:id', async (req, res) => {
+    //route to render 1 team
+});
+
+router.get('/user/:username', async (req, res) => {
+    //route to render user profile
+});
+
+router.get('/schedule', async (req, res) => {
+    //route to render user profile
+});
+
+router.get('/signup', (req, res) => {
+    //route to render signup page
 });
 
 router.get('/login', (req, res) => {

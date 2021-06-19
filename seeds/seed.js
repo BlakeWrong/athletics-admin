a912e7c6c0ff0d9bf255785662e9e1aaff7c162c
gannacon <cwgannaway@gmail.com>
2021-06-19 13:36:58 -0500

    DO NOT MERGE THIS

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,22 +1,27 @@
 const sequelize = require('../config/connection');
-const { User, Team, Role } = require('../models');
+const { User, Team, Role, UserRole } = require('../models');
 
 const userData = require('./userData.json');
 const roleData = require('./roleData.json');
 const teamData = require('./teamData.json');
+const userRoleData = require('./userRoleData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-  Team.bulkCreate(teamData, {
+  const teams = await Team.bulkCreate(teamData, {
     individualHooks: true,
     returning: true,
   });
-  Role.bulkCreate(roleData, {
+  const roles = await Role.bulkCreate(roleData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const userRoles = await UserRole.bulkCreate(userRoleData, {
     individualHooks: true,
     returning: true,
   });

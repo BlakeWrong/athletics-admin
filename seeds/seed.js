a4cd72c5ec7af73c44f6d3916c3c2db66e191b6f
gannacon <cwgannaway@gmail.com>
2021-06-22 19:14:54 -0500

    configure seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,19 +4,24 @@ const { User, Team, Role, UserRole } = require('../models');
 const userData = require('./userData.json');
 const roleData = require('./roleData.json');
 const teamData = require('./teamData.json');
+const userRoleData = require('./userRoleData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-  const teams = await Team.bulkCreate(teamData, {
+  await Team.bulkCreate(teamData, {
     individualHooks: true,
     returning: true,
   });
-  const roles = await Role.bulkCreate(roleData, {
+  await Role.bulkCreate(roleData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await UserRole.bulkCreate(userRoleData, {
     individualHooks: true,
     returning: true,
   });
